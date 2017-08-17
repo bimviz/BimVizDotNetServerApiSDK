e48e5cf9402025f24104932d660bfbf5ef3595a3
zhangxinfeng <zhangxinfeng@scene3d.com>
2017-08-17 12:56:19 +0800

    增加项目缩略图

--- BimViz.ServerApi.Client/Model/ProjectInfo.cs
+++ BimViz.ServerApi.Client/Model/ProjectInfo.cs
@@ -8,6 +8,7 @@ namespace BimViz.ServerApi.Client.Model
     public class ProjectInfo
     {
         public BVProject Project { get; set; }
+        public string ThrumbnailUrl { get; set; } //创建项目时，不需要赋值
         public List<ProjectFile> FileInfos { get; set; }//创建项目时，不需要赋值
         public ProjectSettings SceneSettings { get; set; }//创建项目时，改值可以为空
     }
